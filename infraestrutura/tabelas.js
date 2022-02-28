13d62829872d2cc75e131624b7f9113b463e6088
Daniel de Andrade Souza <daniel.android.andrade@gmail.com>
2022-02-28 16:50:30 -0300

    codigo copiado

---
+++ infraestrutura/tabelas.js
@@ -0,0 +1,34 @@
+class Tabelas {
+
+    init(conexao){
+       this.conexao = conexao
+
+       this.criarClientes()
+    }
+
+    criarClientes(){
+        const sql = 'CREATE TABLE `Oficina`.`Clientes` ( \
+            `idClientes` INT NOT NULL AUTO_INCREMENT, \
+            `nome` VARCHAR(45) NOT NULL, \
+            `cpf` VARCHAR(45) NULL, \
+            `cnpj` VARCHAR(45) NULL, \
+            `telefone1` INT NULL, \
+            `telefone2` INT NULL, \
+            `telefone3` INT NULL, \
+            `celular` INT NOT NULL, \
+            `email` VARCHAR(45) NULL, \
+            `endereco` VARCHAR(45) NULL ,\
+            PRIMARY KEY (`idClientes`)); \
+          '
+
+        this.conexao.query(sql, erro => {
+            if (erro) {
+                console.log(erro)
+            } else{
+                console.log('Tabela Clientes criada com sucesso')
+            }
+        })
+    }
+}
+
+module.exports = new Tabelas
