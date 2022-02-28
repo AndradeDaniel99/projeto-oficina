5a4368e0e6e54deaeb33edbe69cb652065a7d7e9
BorgesMath <matheusborgessampaio@gmail.com>
2022-02-28 18:03:31 -0300

    autoincrement criarcarros, tabeja.js

--- infraestrutura/tabelas.js
+++ infraestrutura/tabelas.js
@@ -33,7 +33,7 @@ class Tabelas {
 
     criarCarros(){
         const sql = 'CREATE TABLE IF NOT EXISTS `oficina`.`carros` (\
-            `idCarros` INT NOT NULL,\
+            `idCarros` INT NOT NULL AUTO_INCREMENT,\
             `PLACA` VARCHAR(8) NOT NULL,\
             `ANO` YEAR(4) NULL,\
             `COR` VARCHAR(10) NULL,\
