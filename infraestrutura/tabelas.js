20f830e224856139e0b855d8c1d40102bb7a6539
BorgesMath <matheusborgessampaio@gmail.com>
2022-02-28 17:58:01 -0300

    if exists, tabelas.js

--- infraestrutura/tabelas.js
+++ infraestrutura/tabelas.js
@@ -3,6 +3,7 @@ class Tabelas {
     init(conexao){
        this.conexao = conexao
 
+       this.criarCarros() 
        this.criarClientes()
     }
 
@@ -19,6 +20,28 @@ class Tabelas {
             `email` VARCHAR(45) NULL, \
             `endereco` VARCHAR(45) NULL ,\
             PRIMARY KEY (`idClientes`)); \
+          '
+
+        this.conexao.query(sql, erro => {
+            if (erro) {
+                console.log(erro)
+            } else{
+                console.log('Tabela Clientes criada com sucesso')
+            }
+        })
+    }
+
+    criarCarros(){
+        const sql = 'CREATE TABLE `oficina`.`carros` (\
+            `idCarros` INT NOT NULL,\
+            `PLACA` VARCHAR(8) NOT NULL,\
+            `ANO` YEAR(4) NULL,\
+            `COR` VARCHAR(10) NULL,\
+            `MARCA` VARCHAR(15) NULL,\
+            `MODELO` VARCHAR(15) NULL,\
+            PRIMARY KEY (`idCarros`))\
+          ENGINE = InnoDB\
+          DEFAULT CHARACTER SET = utf8;\
           '
 
         this.conexao.query(sql, erro => {
