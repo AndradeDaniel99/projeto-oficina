161b669229d3ea390b8e5b4f5e16200f5d7dac50
Daniel de Andrade Souza <daniel.android.andrade@gmail.com>
2022-03-01 10:40:49 -0300

    metodos iniciais da classe order implementados

--- models/order.js
+++ models/order.js
@@ -3,7 +3,73 @@ const moment = require('moment');
 
 class Order {
 
+
     
+
+
+    adiciona(order, res){
+
+        if (erros) {
+            res.status(400).json(erros)
+        } else{
+
+            const sql = 'INSERT INTO order SET ?'
+
+            conexao.query(sql, order, (erro, resultados)=>{
+                if (erro) {
+                    res.status(400).json(erro)
+                } else{
+                    res.status(201).json({order})
+                }
+            })
+        }
+    }
+
+    lista(res){
+        const sql = 'SELECT * FROM order'
+        conexao.query(sql, (erro, resultados)=>{
+            if (erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(resultados)
+            }
+        })
+    }
+
+    buscaPorId(id, res){
+        const sql = 'SELECT * FROM order WHERE id = ' + id
+        conexao.query(sql, (erro, resultados)=>{
+            if (erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json({id})
+            }
+        })
+    }
+
+    altera(id, valores, res){
+
+        const sql = 'UPDATE order SET ? WHERE id=?'
+        conexao.query(sql, [valores, id], (erro, resultados)=>{
+            if (erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json({...valores, id})
+            }
+        })
+    }
+
+    deleta(id, res){
+        const sql = 'DELETE FROM order WHERE id = ' + id
+
+        conexao.query(sql, (erro, resultados)=>{
+            if (erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json({id})
+            }
+        })
+    }
 
 
 }
