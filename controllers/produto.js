cc7ae81bac9892d4133e305ca9e8032f26703348
Daniel de Andrade Souza <daniel.android.andrade@gmail.com>
2022-03-01 00:58:17 -0300

    criado classe produto

---
+++ controllers/produto.js
@@ -0,0 +1,31 @@
+const Fornecedor = require('../models/fornecedor')
+
+module.exports = app => {
+    app.get('/fornecedor', (req, res)=> {
+        Fornecedor.lista(res)
+    })
+
+    app.get('/fornecedor/:id', (req, res)=> {
+        const id = parseInt(req.params.id)
+        const valores = req.body
+        Fornecedor.buscaPorId(id, res)
+    })
+
+    app.post('/fornecedor', (req, res)=> {
+        console.log(req.body)
+        const fornecedor = req.body
+        Fornecedor.adiciona(fornecedor, res)
+    })
+
+    app.put('/fornecedor/:id', (req, res)=> {
+        const id = parseInt(req.params.id)
+        const valores = req.body
+        Fornecedor.altera(id, valores, res)
+    })
+
+    app.delete('/fornecedor/:id', (req, res)=>{
+        const id = parseInt(req.params.id)
+        
+        Fornecedor.deleta(id, res)
+    })
+}
