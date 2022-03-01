4cab57628a6d4ffaaf0177000d0d246d145cff69
Daniel de Andrade Souza <daniel.android.andrade@gmail.com>
2022-03-01 16:35:51 -0300

    produto controller arrumado

--- controllers/produto.js
+++ controllers/produto.js
@@ -2,13 +2,13 @@ const Produto = require('../models/produto')
 
 module.exports = app => {
     app.get('/produto', (req, res)=> {
-        Produto.lista(res)
+        Produto.lista(Produto.tabela, res)
     })
 
     app.get('/produto/:id', (req, res)=> {
         const id = parseInt(req.params.id)
         const valores = req.body
-        Produto.buscaPorId(id, res)
+        Produto.buscaPorId(id, Produto.tabela, res)
     })
 
     app.post('/produto', (req, res)=> {
@@ -20,12 +20,18 @@ module.exports = app => {
     app.put('/produto/:id', (req, res)=> {
         const id = parseInt(req.params.id)
         const valores = req.body
-        Produto.altera(id, valores, res)
+        Produto.altera(id, valores, Produto.tabela, res)
+    })
+
+    app.patch('/produto/:id', (req, res)=> {
+        const id = parseInt(req.params.id)
+        const valores = req.body
+        Produto.altera(id, valores, Produto.tabela, res)
     })
 
     app.delete('/produto/:id', (req, res)=>{
         const id = parseInt(req.params.id)
         
-        Produto.deleta(id, res)
+        Produto.deleta(id, Produto.tabela, res)
     })
 }
